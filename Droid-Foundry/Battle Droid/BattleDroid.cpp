1a3f3d836ae6e022b8e4e9911046ba7d7aad1063
xerogrid <brendan@clodfelter.io>
2025-02-12 21:19:05 -0500

    First good working BattleDroid Version

--- Droid-Foundry/Battle Droid/BattleDroid.cpp
+++ Droid-Foundry/Battle Droid/BattleDroid.cpp
@@ -14,7 +14,8 @@ const int verticalHomePosition   = 90;
 const int horizontalHomePosition = 90;
 const int horizontalLeftLimit    = 20;
 const int horizontalRightLimit   = 160;
-const int verticalUpLimit        = 160;  // Adjust as needed
+const int verticalUpLimit        = 160;  // Maximum vertical angle
+const int verticalDownLimit      = 20;   // Minimum vertical angle
 
 // ------------------------------
 // Object Declarations
@@ -40,7 +41,6 @@ void setup() {
   // Initialize the IR receiver (IRremote 4.x+)
   IrReceiver.begin(irReceiverPin, ENABLE_LED_FEEDBACK);
   
-  // Optional: Print a startup message for debugging
   Serial.println("BattleDroid template initialized.");
 }
 
@@ -50,59 +50,86 @@ void setup() {
 void loop() {
   // Check if an IR code has been received
   if (IrReceiver.decode()) {
+    uint32_t fullCode = IrReceiver.decodedIRData.decodedRawData;
+    Serial.print("IR code received: 0x");
+    Serial.println(fullCode, HEX);
     
     // * Button to home servos
-    if (IrReceiver.decodedIRData.command == 0x00FF30CF) {
+    if (fullCode == 0xE916FF00) {
+      Serial.println("Homing servos");
       verticalServo.write(verticalHomePosition);
       horizontalServo.write(horizontalHomePosition);
     }
-    // Up Button to move up
-    else if (IrReceiver.decodedIRData.command == 0x00FF18E7) {
-      int newPosition = verticalServo.read() - 10;
-      if (newPosition > verticalUpLimit) {
-        verticalServo.write(newPosition);
-      }
-    }
-    // Down Button to move down
-    else if (IrReceiver.decodedIRData.command == 0x00FF4AB5) {
+    // Up Button to move up (increase vertical angle)
+    else if (fullCode == 0xE718FF00) {
       int newPosition = verticalServo.read() + 10;
-      if (newPosition < verticalHomePosition) {
+      if (newPosition <= verticalUpLimit) {
+        Serial.print("Moving vertical servo up to ");
+        Serial.println(newPosition);
         verticalServo.write(newPosition);
+      } else {
+        Serial.println("Vertical up limit reached.");
       }
     }
-    // Left Button to move left
-    else if (IrReceiver.decodedIRData.command == 0x00FF10EF) {
+    // Down Button to move down (decrease vertical angle)
+    else if (fullCode == 0xAD52FF00) {
+      int newPosition = verticalServo.read() - 10;
+      if (newPosition >= verticalDownLimit) {
+        Serial.print("Moving vertical servo down to ");
+        Serial.println(newPosition);
+        verticalServo.write(newPosition);
+      } else {
+        Serial.println("Vertical down limit reached.");
+      }
+    }
+    // Left Button to move left (decrease horizontal angle)
+    else if (fullCode == 0xF708FF00) {
       int newPosition = horizontalServo.read() - 10;
-      if (newPosition > horizontalLeftLimit) {
+      if (newPosition >= horizontalLeftLimit) {
+        Serial.print("Moving horizontal servo left to ");
+        Serial.println(newPosition);
         horizontalServo.write(newPosition);
+      } else {
+        Serial.println("Horizontal left limit reached.");
       }
     }
-    // Right Button to move right
-    else if (IrReceiver.decodedIRData.command == 0x00FF5AA5) {
+    // Right Button to move right (increase horizontal angle)
+    else if (fullCode == 0xA55AFF00) {
       int newPosition = horizontalServo.read() + 10;
-      if (newPosition < horizontalRightLimit) {
+      if (newPosition <= horizontalRightLimit) {
+        Serial.print("Moving horizontal servo right to ");
+        Serial.println(newPosition);
         horizontalServo.write(newPosition);
+      } else {
+        Serial.println("Horizontal right limit reached.");
       }
     }
     // 0 button to home vertical servo
-    else if (IrReceiver.decodedIRData.command == 0x00FF38C7) {
+    else if (fullCode == 0xE619FF00) {
+      Serial.println("Homing vertical servo");
       verticalServo.write(verticalHomePosition);
     }
     // 8 button to home horizontal servo
-    else if (IrReceiver.decodedIRData.command == 0x00FF42BD) {
+    else if (fullCode == 0xEA15FF00) {
+      Serial.println("Homing horizontal servo");
       horizontalServo.write(horizontalHomePosition);
     }
     // 1 button to randomly sweep both servos
-    else if (IrReceiver.decodedIRData.command == 0x00FF52AD) {
-      int verticalPosition = random(verticalHomePosition, verticalUpLimit);
-      int horizontalPosition = random(horizontalLeftLimit, horizontalRightLimit);
+    else if (fullCode == 0xBA45FF00) {
+      int verticalPosition = random(verticalDownLimit, verticalUpLimit + 1);
+      int horizontalPosition = random(horizontalLeftLimit, horizontalRightLimit + 1);
+      Serial.print("Random positions: Vertical=");
+      Serial.print(verticalPosition);
+      Serial.print(", Horizontal=");
+      Serial.println(horizontalPosition);
       verticalServo.write(verticalPosition);
       horizontalServo.write(horizontalPosition);
     }
-
+    else {
+      Serial.println("Undefined IR command.");
+    }
+    
     // Resume IR receiver to get ready for the next signal
     IrReceiver.resume();
   }
-
-  // TODO: Optionally add additional code here
 }
