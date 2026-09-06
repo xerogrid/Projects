52a76d7bb9468e38858629d24ad05541f3920e0a
xerogrid <brendan@clodfelter.io>
2026-09-06 09:35:50 -0400

    Wait for Wayland before launching kiosk

--- Digital-Signage-Appliance/start-kiosk.sh
+++ Digital-Signage-Appliance/start-kiosk.sh
@@ -4,4 +4,9 @@ export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$UID_NUM}"
 export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
 export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=/run/user/$UID_NUM/bus}"
 export GDK_BACKEND=wayland
+
+while [ ! -S "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" ]; do
+  sleep 1
+done
+
 exec /opt/digital-signage/bin/kiosk.sh
